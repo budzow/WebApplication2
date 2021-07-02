341b62cdc0416bf2caf8302cead3a0b030a7734b
Michal Budzowski <michal.budzowski@sonarsource.com>
2021-07-02 10:10:23 +0200

    removed build scripts with tokens

--- build.bat
+++
@@ -1,3 +0,0 @@
-"D:\sonar-scanner-msbuild-5.0.0.23533-net46\SonarScanner.MSBuild.exe" begin /k:"WebApplication2" /d:"sonar.host.url=http://localhost:8000" /d:sonar.password=sonar /d:sonar.login=sonar
-msbuild.exe /t:rebuild
-"D:\sonar-scanner-msbuild-5.0.0.23533-net46\SonarScanner.MSBuild.exe" end /d:sonar.login=sonar /d:sonar.password=sonar
--- build_sc.bat
+++
@@ -1,3 +0,0 @@
-"D:\sonar-scanner-msbuild-5.0.0.23533-net46\SonarScanner.MSBuild.exe" begin /k:"WebApplication2-" /o:"budzow-github" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="73c56cf51072a07dee47c081e20b460d51ca6e4c"
-msbuild.exe /t:rebuild
-"D:\sonar-scanner-msbuild-5.0.0.23533-net46\SonarScanner.MSBuild.exe" end /d:sonar.login="73c56cf51072a07dee47c081e20b460d51ca6e4c"
